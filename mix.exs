8abc7b12299e6927b19a4d10ba4c1fc408e737a8
relax2code <relax2code@gmail.com>
2021-07-11 18:11:23 +0700

    add rename lib

--- mix.exs
+++ mix.exs
@@ -46,7 +46,10 @@ defmodule BasePhx.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+
+      #rename
+      {:rename, "~> 0.1.0", only: :dev}
     ]
   end
 
