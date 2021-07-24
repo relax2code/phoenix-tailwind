103a2f2e5e9a0542fed72eff3abb138dbd7a8b65
relax2code <relax2code@gmail.com>
2021-07-24 17:39:16 +0700

    alpine v3

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,8 +17,21 @@ import {Socket} from "phoenix"
 import topbar from "topbar"
 import {LiveSocket} from "phoenix_live_view"
 
+import Alpine from "alpinejs";
+window.Alpine = Alpine
+Alpine.start();
+
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+let liveSocket = new LiveSocket("/live", Socket, {
+    params: {_csrf_token: csrfToken},
+    dom: {
+        onBeforeElUpdated(from, to){
+            if(from._x_dataStack){
+                window.Alpine.clone(from, to);
+            }
+        }
+    }
+})
 
 // Show progress bar on live navigation and form submits
 topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
